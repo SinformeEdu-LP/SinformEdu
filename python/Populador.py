5446e1672498f218495d886ce69e3e295fdab14a
JobsonRP <jobsonrp@gmail.com>
2018-05-31 23:55:33 -0300

    Restante dos FORs e o outro WHILE substituídos por MAP

--- python/Populador.py
+++ python/Populador.py
@@ -38,7 +38,6 @@ DEPUTADOS_CONFIG = {
     'insert_command': "INSERT"
 }
 
-
 def preprocess_proposta_csv_row(csv_rows):
     desc_orgao_sup = FORMAT_REMOVE_ACCENTS(csv_rows[5])
 
@@ -307,33 +306,42 @@ OBTV_CONVENENTE_CONFIG = {
     'insert_command': "INSERT"
 }
 
+def columns_name_statement(columns_to_insert):
+    insert_columns_name_statement = '(' + ','.join(list(map(lambda x: '`' + x + '`', columns_to_insert))) + ')'
+    return insert_columns_name_statement
+
+def rangesBat(num):
+    return [num * DEFAULT_BATCH_SIZE, num * DEFAULT_BATCH_SIZE + DEFAULT_BATCH_SIZE]
+
+
+def mapInsertDB(db_cursor, table_name, insert_command, insert_values, insert_columns_name_statement, total_rows, ranges):
+    batch_start = timeit.default_timer()
+    insert_values_batch = insert_values[ranges[0]:ranges[1]]
+    insert_sql_command = insert_command + ' INTO ' + table_name + ' ' + insert_columns_name_statement + ' VALUES ' + ', '.join(insert_values_batch)
+    db_cursor.execute(insert_sql_command)
+    db.commit()
+    batch_stop = timeit.default_timer()
+    batch_time = batch_stop - batch_start
+    print("Linhas inseridas/atualizadas em " + table_name + ": " + str(ranges[1]) + "/" + str(total_rows) + ' (' + str(batch_time) + 's)')
 
 def insert_values_on_database(db_cursor, table_name, insert_command, columns_to_insert, insert_values):
     start = timeit.default_timer()
-    insert_columns_name_statement = '(' + ','.join(
-        [('`' + column_name + '`') for column_name in columns_to_insert]) + ')'
-    inserted_rows = 0
+    insert_columns_name_statement = columns_name_statement(columns_to_insert)
+    
     total_rows = len(insert_values)
-    while inserted_rows < len(insert_values):
-        batch_start = timeit.default_timer()
+    print('total_rows: ',total_rows)
+    
+    numBatchs = total_rows // DEFAULT_BATCH_SIZE
+    lastRangeLow = total_rows - (total_rows % DEFAULT_BATCH_SIZE) 
 
-        next_batch_size = min(DEFAULT_BATCH_SIZE, len(insert_values) - inserted_rows)
-        batch_range_lower_index = inserted_rows
-        batch_range_higher_index = inserted_rows + next_batch_size
-        insert_values_batch = insert_values[batch_range_lower_index:batch_range_higher_index]
+    listSequenceBatchs = list(range(numBatchs))
 
-        insert_sql_command = insert_command + ' INTO ' + table_name + ' ' + insert_columns_name_statement \
-                             + ' VALUES ' + ', '.join(insert_values_batch)
-        # print(insert_sql_command)
-
-        db_cursor.execute(insert_sql_command)
-        inserted_rows += next_batch_size
-        db.commit()
-
-        batch_stop = timeit.default_timer()
-        batch_time = batch_stop - batch_start
-        print("Linhas inseridas/atualizadas em " + table_name + ": " + str(inserted_rows) + "/" + str(
-            total_rows) + ' (' + str(batch_time) + 's)')
+    listRangesBatchs = list(map(lambda x: rangesBat(x) , listSequenceBatchs))
+    listRangesBatchs.append([lastRangeLow, total_rows])
+    print(listRangesBatchs)
+    
+    list(map(lambda x: mapInsertDB(db_cursor, table_name, insert_command, insert_values, insert_columns_name_statement, total_rows, x) , listRangesBatchs))
+    
     stop = timeit.default_timer()
     time_spent = stop - start
     print('Tempo inserindo na tabela ' + table_name + ': ' + str(time_spent) + 's')
@@ -356,6 +364,14 @@ def filter_csv_row(csv_row, csv_columns_indexes):
  
     return filtered_csv_row
 
+def require_not_null_condition(csv_row, index):
+    if csv_row[index] == '':
+        raise (Exception('Erro - condicao csv_require_not_null_indexes nao satisfeita')) 
+
+def require_not_null(csv_row, csv_require_not_null_indexes):
+    filtered_csv_row = list(map(lambda x: require_not_null_condition(csv_row, x), csv_require_not_null_indexes))
+    return filtered_csv_row
+
 def mapInserirValoresBD(row_formatters, insert_value_format, csv_columns_indexes, allow_null_columns, csv_require_not_null_indexes, csv_preprocess_row, insert_values, reader):
     list(map(lambda z: inserirValoresBD(row_formatters, insert_value_format, csv_columns_indexes, allow_null_columns, csv_require_not_null_indexes, csv_preprocess_row, insert_values,z), reader))
 
@@ -368,15 +384,13 @@ def inserirValoresBD(row_formatters, insert_value_format, csv_columns_indexes, a
             print(str(error))
     if csv_require_not_null_indexes != None:
         try:
-            for index in csv_require_not_null_indexes:
-                if csv_row[index] == '':
-                    raise (Exception('Erro - condicao csv_require_not_null_indexes nao satisfeita'))     
+            require_not_null(csv_row, csv_require_not_null_indexes)
         except:
-            print("erro")
+            print("erro: csv_require_not_null_indexes")
     if csv_row != []:
         csv_row = filter_csv_row(csv_row, csv_columns_indexes)
         if not allow_null_columns and '' in csv_row:
-            print("erro")
+            print("erro: filter_csv_row")
         #if allow_null_columns or '' not in csv_row:
         else:
             insert_values.append(build_insert_value(csv_row, row_formatters, insert_value_format))
@@ -395,9 +409,6 @@ def convert_csv_to_sql_insert_values(config):
 
     ifile = open('arquivos/' + file_name + '.csv', 'r', encoding="utf-8")
     reader = csv.reader(ifile, delimiter=';')
-
-    insert_values = list()
-    
     reader = list(reader)
 
     header = ';'.join(reader[0])
@@ -407,6 +418,7 @@ def convert_csv_to_sql_insert_values(config):
     
     reader = reader[1:]
     
+    insert_values = list()
     mapInserirValoresBD(row_formatters, insert_value_format, csv_columns_indexes, allow_null_columns, csv_require_not_null_indexes, csv_preprocess_row, insert_values, reader)
 
     return insert_values
@@ -419,9 +431,7 @@ def process(db_cursor, config):
     print('Entrada processada para tabela ' + config['table_name'] + ' em: ' + str(
         stop_build_insert - start_build_insert) + 's')
 
-    insert_values_on_database(db_cursor, config['table_name'], config['insert_command'], config['columns_to_insert'],
-                              insert_values_sql_part)
-
+    insert_values_on_database(db_cursor, config['table_name'], config['insert_command'], config['columns_to_insert'], insert_values_sql_part)
 
 db = None
 
